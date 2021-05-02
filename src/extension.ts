56a87d438f0c3a77181b4ae131965ff17de9acce
Seiyial Aurum <sayhao@seiyial.xyz>
2021-05-02 14:13:45 +0800

    Works for objects; will do arrays and function calls next

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+
+const objectOuterRegexp = new RegExp(/^(\s)*((\w+) (\w)(:( )?(\w)+)?( )?=)?(\s)*([{])((.)*)([}])(\s)*$/)
+// const arrayOuterRegexp
+
+const innerRegexp = new RegExp(/^((([\w\d_]+)|(['][\w\d_]+['])|(["][\w\d_]+["])):(\s)*(.)*(,)?)*$/)
+const WHITESPACE = '	'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-expression-multiliner-js-ts" is now active!');
+	const COMMAND: string = 'expression-multiliner-js-ts.toggle'
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-expression-multiliner-js-ts.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	console.log('Congratulations, your extension "expression-multiliner-js-ts" is now active!')
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Expression Multiliner (JS/TS)!');
-	});
+	let disposable = vscode.commands.registerCommand('expression-multiliner-js-ts.helloWorld', () => {
+		const editor = vscode.window.activeTextEditor
 
-	context.subscriptions.push(disposable);
+		if (!editor) {
+			return
+		}
+
+		const { selection, document } = editor
+		const firstLineOfSelection = document.lineAt(selection.start.line)
+		const lastLineOfSelection = document.lineAt(selection.end.line)
+		const fullSelection = new vscode.Range(
+			firstLineOfSelection.range.start,
+			lastLineOfSelection.range.end
+		)
+		const text = document.getText(fullSelection)
+
+		const matchObject = objectOuterRegexp.exec(text)
+		if (matchObject) {
+			const content: string = matchObject[11].trim()
+			const keyValues = content.split(',')
+			const newContent = keyValues.map((kv) => kv.trim()).join(`,\r\n${WHITESPACE}`)
+			editor.edit((editBuilder) => editBuilder.replace(fullSelection, `${matchObject[2]} ${matchObject[10]}\r\n${WHITESPACE}${newContent}\r\n${matchObject[13]}`))
+			// editor.
+			return
+		}
+		// const matchArray = arrayOuterRegexp.exec(text)
+	})
+
+	context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
