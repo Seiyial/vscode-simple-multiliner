9cdec61efd762e9f5f434cba2240068f48c0259f
Seiyial Aurum <sayhao@seiyial.xyz>
2021-07-06 19:51:15 +0800

    Support all statements with fuzzy match

--- README.md
+++ README.md
@@ -1,70 +1,44 @@
-# vscode-expression-multiliner-js-ts README
+# Simple expression multiliner (beta)
 
-This is the README for your extension "vscode-expression-multiliner-js-ts". After writing up a brief description, we recommend including the following sections.
+### What it does
 
-## Features
+Turns this:
+```dart
+// dart
+final bundle = APIMessageBundle(messages: messages, title: title, mostRecentMsgDT: mostRecentMsgDT, type: type);
+```
+```ts
+// JS/TS
+const a: B = {c: 'd', e: 'f'}
+const someFn = (arg1: string, arg2: number, arg3: User) => {}
+```
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Into This:
+```dart
+// dart
+final bundle = APIMessageBundle(
+	messages: messages,
+	title: title,
+	mostRecentMsgDT: mostRecentMsgDT,
+	type: type
+);
+```
+```ts
+// JS/TS
+const a: B = {
+	c: 'd',
+	e: 'f'
+}
+const someFn = (
+	arg1: string,
+	arg2: number,
+	arg3: User
+) => {
 
-For example if there is an image subfolder under your extension project workspace:
+}
+```
 
-\!\[feature X\]\(images/feature-x.png\)
+### What it doesn't do (yet)
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- Discriminate which blocks are meant to be multilined and which aren't. Essentially, may not be able to handle complex statements.
+- Pull requests that help it support more complex statements/features without hurting existing support will all be welcome!
--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,23 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
-const objectOuterRegexp = new RegExp(/^(\s)*((\w+) (\w)(:( )?(\w)+)?( )?=)?(\s)*([{])((.)*)([}])(\s)*$/)
-// const arrayOuterRegexp
-
-const innerRegexp = new RegExp(/^((([\w\d_]+)|(['][\w\d_]+['])|(["][\w\d_]+["])):(\s)*(.)*(,)?)*$/)
-const WHITESPACE = '	'
+// const objectOuterRegexp = new RegExp(/^((\w+) (\w+)(:( )?(\w)+)?( )?=)?(\s)*([{])((.)*)([}])$/)
+// const arrayOuterRegexp = new RegExp(/^((\w+) (\w+)(:( )?(\w)+)?( )?=)?(\s)*([\[])((.)*)([\]])$/)
+// const fnRegexp = new RegExp(/^((public|private)( ))?(readonly )?(async )?$/)
+// // const myFn: xxxxxx = (<-->) => { ... }
+// // const myFn = (<-->): XXXXX => { ... }
+// // function myFn (<-->)(:( )?(\w)+)?( )?([{](.)*[}])
+// // 
+// const innerRegexp = new RegExp(/^((([\w\d_]+)|(['][\w\d_]+['])|(["][\w\d_]+["])):(\s)*(.)*(,)?)*$/)
+// const WHITESPACE = '	'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const COMMAND: string = 'expression-multiliner-js-ts.toggle'
+	const COMMAND: string = 'daybreak-simple-multiliner.spread'
 
-	console.log('Congratulations, your extension "expression-multiliner-js-ts" is now active!')
-
-	let disposable = vscode.commands.registerCommand('expression-multiliner-js-ts.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand(COMMAND, () => {
 		const editor = vscode.window.activeTextEditor
 
 		if (!editor) {
@@ -24,27 +26,49 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		const { selection, document } = editor
-		const firstLineOfSelection = document.lineAt(selection.start.line)
-		const lastLineOfSelection = document.lineAt(selection.end.line)
 		const fullSelection = new vscode.Range(
-			firstLineOfSelection.range.start,
-			lastLineOfSelection.range.end
+			document.lineAt(selection.start.line).range.start,
+			document.lineAt(selection.end.line).range.end
 		)
-		const text = document.getText(fullSelection)
+		const text = document.getText(fullSelection).trimEnd()
 
-		const matchObject = objectOuterRegexp.exec(text)
-		if (matchObject) {
-			const content: string = matchObject[11].trim()
-			const keyValues = content.split(',')
-			const newContent = keyValues.map((kv) => kv.trim()).join(`,\r\n${WHITESPACE}`)
-			editor.edit((editBuilder) => editBuilder.replace(fullSelection, `${matchObject[2]} ${matchObject[10]}\r\n${WHITESPACE}${newContent}\r\n${matchObject[13]}`))
-			// editor.
-			return
-		}
-		// const matchArray = arrayOuterRegexp.exec(text)
+		const { whitespaceChars, numBaseIndents } = _getWhitespace(editor.options, text)
+		const baseIndent = whitespaceChars.repeat(numBaseIndents)
+		const extraIndent = whitespaceChars.repeat(numBaseIndents + 1)
+		editor.edit((eb) => {
+			editor.selection = new vscode.Selection(fullSelection.start, fullSelection.end)
+			const newVal = text.replace(/[{}()]|(, )/g, (c) => c === '}' || c === ')' || c === ']' ? `\r\n${baseIndent}${c}` : `${c.trimEnd()}\r\n${extraIndent}`)
+			eb.replace(fullSelection, newVal)
+		}).then(() => {
+			// editor seems to set new rows only after edit procedure is done; moving the cursor before the 'then' block results in weird behaviour
+			editor.selection = new vscode.Selection(editor.selection.end, editor.selection.end)
+		})
+		return
 	})
 
 	context.subscriptions.push(disposable)
+}
+
+const _getWhitespace = (editorOptions: vscode.TextEditorOptions, text: string): {whitespaceChars: string, numBaseIndents: number} => {
+	const whitespaceChar = editorOptions.insertSpaces && typeof editorOptions.tabSize === 'number'
+		? ' '.repeat(editorOptions.tabSize)
+		: '	'
+	const numIndents = _getNumIndentsRoundedUp(whitespaceChar, text)
+	return {numBaseIndents: numIndents, whitespaceChars: whitespaceChar}
+}
+
+const _getNumIndentsRoundedUp = (whitespaceChar: string, text: string) => {
+	const wscLen = whitespaceChar.length
+	const textLen = text.length
+	const maxIndent = Math.floor(text.length / whitespaceChar.length)
+	for (let i = 0; i < maxIndent; i++) {
+		const pos = wscLen * i
+		if (text.charAt(pos) !== whitespaceChar[0]) {
+			return i
+		}
+		continue
+	}
+	return maxIndent
 }
 
 // this method is called when your extension is deactivated
